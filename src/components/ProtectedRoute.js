c81e4128c7baa50ff4767ca6973b1e6d9ae2dd0d
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-08-17 19:45:35 +0300

    feat: add Content component and ProtectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Redirect, Route} from 'react-router-dom';
 
-function ProtectedRoute(props) {
+function ProtectedRoute({component: Component, ...props}) {
   return (
-    <div>ProtectedRoute</div>
+    <Route>
+      {() =>
+        props.loggedIn ? <Component {...props} /> : <Redirect to="/sign-in" />
+      }
+    </Route>
   );
 }
 
