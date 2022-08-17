5f37ead300c5fc88c5b0d52f4fa8b91cefa18aac
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-08-17 18:12:29 +0300

    feat: add JSX to Login and Register components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,36 @@ import React from 'react';
 
 function Login(props) {
   return (
-    <div>Login</div>
+    <main className="content content_type_second">
+      <h2 className={`popup__title popup__title_type_page`}>Вход</h2>
+      <form className="form" name="login">
+        <label className="form__field">
+          <input type="text"
+                 name="email"
+                 id="email-input"
+                 placeholder="Email"
+                 required
+                 minLength="2"
+                 maxLength="40"
+                 value=""
+                 className="form__text-input form__text-input_type_name form__text-input_type_inverted"/>
+        </label>
+        <label className="form__field form__field_last">
+          <input type="text"
+                 name="password"
+                 id="password-input"
+                 placeholder="Пароль"
+                 required
+                 minLength="2"
+                 maxLength="200"
+                 value=""
+                 className="form__text-input form__text-input_type_job form__text-input_type_inverted"/>
+        </label>
+        <button type="submit"
+                className="form__submit-button form__submit-button_type_inverted">Войти
+        </button>
+      </form>
+    </main>
   );
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 function Register(props) {
   return (
-    <div>Register</div>
+    <main className="content content_type_second">
+      <h2 className={`popup__title popup__title_type_page`}>Регистрация</h2>
+      <form className="form" name="register">
+        <label className="form__field">
+          <input type="text"
+                 name="email"
+                 id="email-input"
+                 placeholder="Email"
+                 required
+                 minLength="2"
+                 maxLength="40"
+                 value=""
+                 className="form__text-input form__text-input_type_name form__text-input_type_inverted"/>
+        </label>
+        <label className="form__field form__field_last">
+          <input type="text"
+                 name="password"
+                 id="password-input"
+                 placeholder="Пароль"
+                 required
+                 minLength="2"
+                 maxLength="200"
+                 value=""
+                 className="form__text-input form__text-input_type_job form__text-input_type_inverted"/>
+        </label>
+        <button type="submit"
+                className="form__submit-button form__submit-button_type_inverted">Зарегистрироваться
+        </button>
+        <div className="form__link-wrap"><NavLink to="/sign-in" className="form__link">Уже зарегистрированы? Войти</NavLink></div>
+      </form>
+    </main>
   );
 }
 
