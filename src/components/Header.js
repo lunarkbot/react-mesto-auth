815b006cae8b6f1c2bb8706197ddb522aee51f75
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-08-17 16:23:22 +0300

    feat: add NavBar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import {NavLink, Route, Switch} from 'react-router-dom';
+import NavBar from './NavBar';
 
 export default function Header() {
   return(
     <header className="header">
       <div className="header__logo"></div>
-      <nav className="header__navbar">
-        <Switch>
-          <Route exact path="/">
+
+      <Switch>
+        <Route exact path="/">
+          <NavBar>
             <NavLink to="/sign-in" className="header__navbar-link">Выйти</NavLink>
-          </Route>
-          <Route path="/sign-up">
+          </NavBar>
+          <div className="header__navbar-menu"></div>
+        </Route>
+        <Route path="/sign-up">
+          <NavBar>
             <NavLink to="/sign-in" className="header__navbar-link">Войти</NavLink>
-          </Route>
-          <Route path="/sign-in">
+          </NavBar>
+        </Route>
+        <Route path="/sign-in">
+          <NavBar>
             <NavLink to="/sign-up" className="header__navbar-link">Регистрация</NavLink>
-          </Route>
-        </Switch>
-      </nav>
+          </NavBar>
+        </Route>
+      </Switch>
     </header>
   )
 }
