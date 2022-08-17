16afb08bdd986630382b5904d8389797467b16fc
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-08-17 22:54:10 +0300

    feat: add jsx to InfoTooltip

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,30 @@
 import React, {useState} from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, withRouter, Link} from 'react-router-dom';
 import auth from '../utils/auth';
+import InfoTooltip from './InfoTooltip';
 
 function Register(props) {
   const [userData, setUserData] = useState({
     email: '',
     password: ''
   });
+
+  const [tooltip, setTooltip] = useState({
+    isOpen: false,
+    isOk: false,
+    isConfirm: false,
+  })
+
+  function handleClose() {
+    setTooltip({
+      ...tooltip,
+      isOpen: false
+    });
+
+    if (tooltip.isOk) {
+      props.history.push('/sign-in');
+    }
+  }
 
   function handleChange(e) {
     const name = e.target.name;
@@ -20,11 +38,16 @@ function Register(props) {
 
   function handleSubmit(e) {
     e.preventDefault();
-
+    console.log(userData)
     auth.signUp(userData)
       .then(res => {
         if (res.ok) {
-          return res.json();
+          setTooltip({
+            ...tooltip,
+            isOpen: true,
+            isOk: true
+          });
+          console.log(tooltip)
         } else {
           let message = '';
 
@@ -39,49 +62,54 @@ function Register(props) {
           return Promise.reject(`Ошибка: ${res.status}. ${message}`);
         }
       })
-      .then(data => {
-        console.log(data);
-      })
       .catch(err => {
         console.log('%c' + err, 'color: #dd3333');
+        setTooltip({
+          ...tooltip,
+          isOpen: true,
+          isOk: false
+        });
       })
   }
 
   return (
-    <main className="content content_type_second">
-      <h2 className={`popup__title popup__title_type_page`}>Регистрация</h2>
-      <form className="form" name="register" onSubmit={handleSubmit}>
-        <label className="form__field">
-          <input type="text"
-                 name="email"
-                 id="email-input"
-                 placeholder="Email"
-                 required
-                 minLength="2"
-                 maxLength="40"
-                 onChange={handleChange}
-                 value={userData.email}
-                 className="form__text-input form__text-input_type_name form__text-input_type_inverted"/>
-        </label>
-        <label className="form__field form__field_last">
-          <input type="password"
-                 name="password"
-                 id="password-input"
-                 placeholder="Пароль"
-                 required
-                 minLength="2"
-                 maxLength="200"
-                 onChange={handleChange}
-                 value={userData.password}
-                 className="form__text-input form__text-input_type_job form__text-input_type_inverted"/>
-        </label>
-        <button type="submit"
-                className="form__submit-button form__submit-button_type_inverted">Зарегистрироваться
-        </button>
-        <div className="form__link-wrap"><NavLink to="/sign-in" className="form__link">Уже зарегистрированы? Войти</NavLink></div>
-      </form>
-    </main>
+    <>
+      <main className="content content_type_second">
+        <h2 className={`popup__title popup__title_type_page`}>Регистрация</h2>
+        <form className="form" name="register" onSubmit={handleSubmit}>
+          <label className="form__field">
+            <input type="text"
+                   name="email"
+                   id="email-input"
+                   placeholder="Email"
+                   required
+                   minLength="2"
+                   maxLength="40"
+                   onChange={handleChange}
+                   value={userData.email}
+                   className="form__text-input form__text-input_type_name form__text-input_type_inverted"/>
+          </label>
+          <label className="form__field form__field_last">
+            <input type="password"
+                   name="password"
+                   id="password-input"
+                   placeholder="Пароль"
+                   required
+                   minLength="2"
+                   maxLength="200"
+                   onChange={handleChange}
+                   value={userData.password}
+                   className="form__text-input form__text-input_type_job form__text-input_type_inverted"/>
+          </label>
+          <button type="submit"
+                  className="form__submit-button form__submit-button_type_inverted">Зарегистрироваться
+          </button>
+          <div className="form__link-wrap"><NavLink to="/sign-in" className="form__link">Уже зарегистрированы? Войти</NavLink></div>
+        </form>
+      </main>
+      <InfoTooltip isOk={tooltip.isOk} isOpen={tooltip.isOpen} onClose={handleClose} />
+    </>
   );
 }
 
-export default Register;
+export default withRouter(Register);
